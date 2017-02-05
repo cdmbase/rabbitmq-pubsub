17348a303f1abf4366eaff3bb725a3ee686847af
veeramarni <mvasireddy@cdmbase.com>
2017-02-04 20:20:19 -0500

    remove direct dependencies

--- source/childLogger.ts
+++
@@ -1,5 +0,0 @@
-import * as bunyan from "bunyan";
-
-export function createChildLogger(logger: bunyan.Logger, className: string) {
-  return logger.child({ child: "rokot-mq-rabbit", "class": className }, true);
-}
--- source/index.ts
+++
@@ -1,4 +0,0 @@
-export {IRabbitMqConnectionFactory,IRabbitMqConnectionConfig,RabbitMqConnectionFactory,RabbitMqSingletonConnectionFactory} from "./connectionFactory";
-export {RabbitMqConsumer,IRabbitMqConsumerDisposer} from "./consumer";
-export {RabbitMqProducer} from "./producer";
-export {IQueueNameConfig} from "./common";
