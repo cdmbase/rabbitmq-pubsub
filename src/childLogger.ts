4559a8cbfeb8ef40a982e4cea951bb031dbf518e
veeramarni <mvasireddy@cdmbase.com>
2017-02-07 15:07:55 -0500

    cleanup

--- src/childLogger.ts
+++ src/childLogger.ts
@@ -1,5 +1,5 @@
 import * as Logger from "bunyan";
 
 export function createChildLogger(logger: Logger, className: string) {
-  return logger.child({ child: "rokot-mq-rabbit", "class": className }, true);
+  return logger.child({ child: "rabbitmq-pub-sub", "class": className }, true);
 }
